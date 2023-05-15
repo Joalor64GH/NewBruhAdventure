600cf9bf5ce0d70506200853fc3536c003c3cf99
khuonghoanghuy <khuonghoanghuy@gmail.com>
2023-05-15 14:30:25 +0700

    added a little thing

--- source/lev/PlayState.hx
+++ source/lev/PlayState.hx
@@ -1,7 +1,23 @@
 package lev;
 
+import Coin.Coin_2;
+import Coin;
+import Player;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.tile.FlxTilemap;
+
 class PlayState extends MainState
 {
+	var player:Player;
+	var coin:FlxTypedGroup<Coin>;
+	var coin_2:FlxTypedGroup<Coin_2>;
+
+	var map:FlxOgmo3Loader;
+	var walls:FlxTilemap;
+
 	override public function create()
 	{
 		super.create();
