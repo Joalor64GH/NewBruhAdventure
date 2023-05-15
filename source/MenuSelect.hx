c6460713f9f683e4269010d84719058897290bd8
khuonghoanghuy <khuonghoanghuy@gmail.com>
2023-05-15 16:32:45 +0700

    lev20

--- source/MenuSelect.hx
+++ source/MenuSelect.hx
@@ -9,6 +9,24 @@ class MenuSelect extends MainSprite
 		loadGraphic(Paths.level_select__png, true, 64, 32);
 		animation.add("lev1", [0]);
 		animation.add("lev2", [1]);
+		animation.add("lev3", [2]);
+		animation.add("lev4", [3]);
+		animation.add("lev5", [4]);
+		animation.add("lev6", [5]);
+		animation.add("lev7", [6]);
+		animation.add("lev8", [7]);
+		animation.add("lev9", [8]);
+		animation.add("lev10", [9]);
+		animation.add("lev11", [10]);
+		animation.add("lev12", [11]);
+		animation.add("lev13", [12]);
+		animation.add("lev14", [13]);
+		animation.add("lev15", [14]);
+		animation.add("lev16", [15]);
+		animation.add("lev17", [16]);
+		animation.add("lev18", [17]);
+		animation.add("lev19", [18]);
+		animation.add("lev20", [19]);
 
 		scale.set(3, 3);
 	}
--- source/MenuSelectLevel.hx
+++ source/MenuSelectLevel.hx
@@ -5,7 +5,10 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 
 class MenuSelectLevel extends MainState
 {
-	var list:Array<String> = ["lev1", "lev2"];
+	var list:Array<String> = [
+		"lev1", "lev2", "lev3", "lev4", "lev5", "lev6", "lev7", "lev8", "lev9", "lev10", "lev11", "lev12", "lev13", "lev14", "lev15", "lev16", "lev17",
+		"lev18", "lev19", "lev20"
+	];
 	var select_lev:FlxTypedGroup<MenuSelect>;
 	var select:Int = 0;
 
@@ -73,6 +76,60 @@ class MenuSelectLevel extends MainState
 
 				case 1:
 					spr.animation.play("lev2");
+
+				case 2:
+					spr.animation.play("lev3");
+
+				case 3:
+					spr.animation.play("lev4");
+
+				case 4:
+					spr.animation.play("lev5");
+
+				case 5:
+					spr.animation.play("lev6");
+
+				case 6:
+					spr.animation.play("lev7");
+
+				case 7:
+					spr.animation.play("lev8");
+
+				case 8:
+					spr.animation.play("lev9");
+
+				case 9:
+					spr.animation.play("lev10");
+
+				case 10:
+					spr.animation.play("lev11");
+
+				case 11:
+					spr.animation.play("lev12");
+
+				case 12:
+					spr.animation.play("lev13");
+
+				case 13:
+					spr.animation.play("lev14");
+
+				case 14:
+					spr.animation.play("lev15");
+
+				case 15:
+					spr.animation.play("lev16");
+
+				case 16:
+					spr.animation.play("lev17");
+
+				case 17:
+					spr.animation.play("lev18");
+
+				case 18:
+					spr.animation.play("lev19");
+
+				case 19:
+					spr.animation.play("lev20");
 			}
 
 			spr.updateHitbox();
