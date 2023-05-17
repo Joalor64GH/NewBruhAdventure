6e83c628f969869690986ba23a14ec01ea260d6d
khuonghoanghuy <khuonghoanghuy@gmail.com>
2023-05-17 21:08:42 +0700

    framerate stuff
    v1.1.0 on progress

--- source/Main.hx
+++ source/Main.hx
@@ -3,15 +3,17 @@ package;
 import flixel.FlxGame;
 import openfl.display.FPS;
 import openfl.display.Sprite;
+import util.Util;
 
 class Main extends Sprite
 {
 	public static var fpsCounter:FPS;
+	public static var framerate:Int = Std.parseInt(Util.fileString(Paths.framerate__txt));
 
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, MenuState));
+		addChild(new FlxGame(0, 0, MenuState, framerate, framerate));
 		fpsCounter = new FPS(0, 0, 0xFFFFFF);
 		addChild(fpsCounter);
 	}
