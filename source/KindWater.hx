fde63c59263622173d113c4e713b4168af71cdd9
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-05-18 09:41:12 +0200

    I think the liquid it’s almost finished

--- source/KindWater.hx
+++ source/KindWater.hx
@@ -3,15 +3,46 @@ package;
 class Liquid extends MainSprite
 {
 	public var killsWhenTouched:Bool = false;
-	public var liquidType /*(default, set)*/:String = '';
+	// In development
+	public var firesUpPlayer:Bool = false;
+	public var slowWalk:Bool = false;
+	
+	public var liquidType(default, set):String = '';
+	inline function set_liquidType(v:String):String
+	{
+		if(liquidType != v)
+		{
+			liquidType = v;
+			reload();
+		}
+		return v;
+	}
 
-	public function new(x:Float = 0, y:Float = 0)
+	public function new(x:Float = 0, y:Float = 0, LiquidType:String = '')
 	{
 		super(x, y);
-
-		loadGraphic(Paths.liquid__png, true, 16, 16);
-		animation.add("posion", [2]);
-		animation.play("posion");
+		this.liquidType = LiquidType;
+	}
+	function reload()
+	{
+		switch(liquidType)
+		{
+			case 'poison':
+				loadGraphic(Paths.liquid__png, true, 16, 16);
+				animation.add("posion", [2]);
+				animation.play("posion");
+				//killsWhenTouched = true;
+			case 'water':
+				loadGraphic(Paths.liquid__png, true, 16, 16);
+				animation.add("water", [1]);
+				animation.play("water");
+				//slowWalk = true;
+			case 'lava':
+				loadGraphic(Paths.liquid__png, true, 16, 16);
+				animation.add("lava", [0]);
+				animation.play("lava");
+				//firesUpPlayer = true;
+		}
 	}
 }
 
