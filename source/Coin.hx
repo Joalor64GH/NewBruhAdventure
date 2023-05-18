30b55bbd8646146ea2038149fb7dac10e25ce7bc
khuonghoanghuy <khuonghoanghuy@gmail.com>
2023-05-18 16:00:52 +0700

    answer question on Coin.hx

--- source/Coin.hx
+++ source/Coin.hx
@@ -7,11 +7,13 @@ class Coin extends MainSprite
 {
 	public var typeCoin(default, set):String = '';
 	public var isFakeCoin:Bool = false;
+
 	/**
 		How much score does the coin gives when touched.
-	*/
+	 */
 	public var score:Int = 10;
-	//Maybe... like... you have to do something to take this coin.
+
+	// Maybe... like... you have to do something to take this coin.
 	public var canBeScored:Bool = true;
 
 	inline function set_typeCoin(v:String):String
@@ -48,6 +50,11 @@ class Coin extends MainSprite
 	{
 		switch (typeCoin)
 		{
+			case 'coin':
+				loadGraphic(Paths.coin__png, true, 16, 16);
+				updateHitbox();
+				score = 10;
+
 			case 'coin_2':
 				loadGraphic(Paths.coin_2__png, true, 16, 16);
 				updateHitbox();
@@ -60,23 +67,30 @@ class Coin extends MainSprite
 				updateHitbox();
 				score = 100;
 
-			case 'coin_fake': // a black coin? A normal coin but with different animation???
+			case 'coin_fake': // a black coin that you will get nothing
+				loadGraphic(Paths.coin_black__png, true, 16, 16);
+				updateHitbox();
 				isFakeCoin = true;
 				score = 0;
 
-			case 'coin_rewarded': // maybe a red one???
+			case 'coin_rewarded': // a red one that have a animtion png
+				loadGraphic(Paths.coin_red__png, true, 16, 16);
+				animation.add('idle', [0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0], 12, true);
+				animation.play('idle');
+				updateHitbox();
 				canBeScored = false;
 		}
 	}
+
 	function playerTouched(player:Player)
 	{
 		this.kill();
 	}
 }
-
 // This must be deprecated.
-class Coin_2 extends MainSprite
-{
+/*
+	class Coin_2 extends MainSprite
+	{
 	public function new(x:Float = 0, y:Float = 0)
 	{
 		super(x, y);
@@ -94,11 +108,11 @@ class Coin_2 extends MainSprite
 	{
 		exists = false;
 	}
-}
+	}
 
-// This is deprecated
-class Coin_Super extends MainSprite
-{
+	// This is deprecated
+	class Coin_Super extends MainSprite
+	{
 	public function new(x:Float = 0, y:Float = 0)
 	{
 		super(x, y);
@@ -118,4 +132,5 @@ class Coin_Super extends MainSprite
 	{
 		exists = false;
 	}
-}
+	}
+ */
