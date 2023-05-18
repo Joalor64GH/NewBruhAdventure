cab5c852e509407a76559341a32e86e0ad431947
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-05-18 20:41:24 +0200

    Update MenuSelect.hx

--- source/MenuSelect.hx
+++ source/MenuSelect.hx
@@ -2,12 +2,14 @@ package;
 
 class MenuSelect extends MainSprite
 {
+	public static var levels:Int = 45;
+	
 	public function new(x:Float = 0, y:Float = 0)
 	{
 		super(x, y);
 
 		loadGraphic(Paths.level_select__png, true, 64, 32);
-		for (i in 0...45)
+		for (i in 0...levels)
 		{
 			animation.add("lev" + Std.string(i + 1), [i]);
 		}
