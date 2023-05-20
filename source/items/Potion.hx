161823fef842a7c7e03bb7bd5566a3ef4d98ef59
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-05-20 13:38:05 +0200

    Update Potion.hx

--- source/items/Potion.hx
+++ source/items/Potion.hx
@@ -1,4 +1,15 @@
 package items;
 
+import flixel.util.FlxColor;
+
 class Potion extends MainSprite {
+	//No, not set.
+	public var potionType:String = '';
+	
+	public function new(x:Float, y:Float, color:FlxColor)
+	{
+		super(x, y);
+		//loadGraphic(potion); // BLANK POTION. WITHOUT ANY COLORS EXCEPT BLACK (border) AND WHITE (in color)
+		this.color = color; // no need to make more sprites for more potion colors.
+	}
 }
