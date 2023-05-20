a92b3d5f82094423b06eda7d8aa3f935a200db4b
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-05-20 17:53:41 +0200

    uughhh....

--- source/mainMenu/MenuStorySelect.hx
+++ source/mainMenu/MenuStorySelect.hx
@@ -30,6 +30,7 @@ class MenuStorySelect extends MainState
 			var selectThing:MenuMode = new MenuMode(300, 0);
 			selectThing.ID = i;
 			selectThing.screenCenter(Y);
+			selectThing.y -= (i * 40);
 			selectThing.scrollFactor.set();
 			menu_group.add(selectThing);
 		}
