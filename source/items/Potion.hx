2ad39e3c79499435bf92a37f8f759a83bebe53ea
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-05-20 13:39:03 +0200

    Update Potion.hx

--- source/items/Potion.hx
+++ source/items/Potion.hx
@@ -2,6 +2,7 @@ package items;
 
 import flixel.util.FlxColor;
 
+//THIS IS AN ITEM FOR THE 2ND ADVENTURE!! NOT THE MAIN ONE!! THIS IS FOR THE UNDERGROUND ONE, SO DONT ADD IT UNTIL I SAY IT.
 class Potion extends MainSprite {
 	//No, not set.
 	public var potionType:String = '';
