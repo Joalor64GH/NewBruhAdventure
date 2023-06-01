bca86e969fbe8fbdea70c52f615d1ab69b9c6288
khuonghoanghuy <khuonghoanghuy@gmail.com>
2023-06-01 21:40:18 +0700

    update alot stuff

--- source/MenuSelect.hx
+++ source/MenuSelect.hx
@@ -1,14 +1,21 @@
 package;
 
+import util.Util;
+
 class MenuSelect extends MainSprite
 {
-	public static var levels:Int = 45;
-	
+	public static var levels:Int;
+
 	public function new(x:Float = 0, y:Float = 0)
 	{
 		super(x, y);
 
+		trace('level animate');
+
+		levels = Std.parseInt(Util.fileString(Paths.numberLevel__txt));
+
 		loadGraphic(Paths.level_select__png, true, 64, 32);
+
 		for (i in 0...levels)
 		{
 			animation.add("lev" + Std.string(i + 1), [i]);
